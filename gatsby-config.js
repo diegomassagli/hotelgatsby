316d16cd8bee7d49e34c3081efc4c68b9d7f0169
Diego Massagli <diegomassagli@gmail.com>
2022-03-08 00:04:53 -0300

    primer commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    'gatsby-plugin-emotion',
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
@@ -28,11 +29,14 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/download.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        apiToken: '24bd59154fb7a8fddf16a11ff6d6d6'
+      }
+    }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,60 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react";
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import Layout from '../components/layout';
+import ImagenHotel from '../components/imagenHotel';
+import ContenidoInicio from '../components/contenidoInicio';
+import HabitacionPreview from "../components/habitacionPreview";
+import useHabitaciones from "../hooks/use-habitaciones";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
 
-const IndexPage = () => (
+const ListadoHabitaciones = styled.ul`
+  max-width: 1200px;
+  width: 95%;
+  margin: 4rem auto 0 auto;
+    
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 3rem;
+  }
+
+  @media (min-width: 1024px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 3rem;
+  }
+`;
+
+const IndexPage = () => {
+  const habitaciones = useHabitaciones();
+  console.log(habitaciones);
+
+return (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    <ImagenHotel />
+
+    <ContenidoInicio />
+    <h2
+      css={css`
+        text-align: center;
+        margin-top: 5rem;
+        font-size: 3rem;
+      `}
+    >Nuestras Habitaciones</h2>
+
+    <ListadoHabitaciones>
+      {habitaciones.map(habitacion => (
+        <HabitacionPreview 
+          key={habitacion.id}
+          habitacion={habitacion}
+        />
+      ))}
+
+    </ListadoHabitaciones>
+
   </Layout>
 )
+}
 
 export default IndexPage
